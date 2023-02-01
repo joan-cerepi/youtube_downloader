a6e4a7460a76ddd63a700e90e6f71595dbff7fb3
Joan Çerepi <slasher2033@protonmail.com>
2023-02-01 16:29:16 +0100

    added video downloading functionality

--- main.py
+++ main.py
@@ -1,22 +1,54 @@
+from menu import option_menu
 import sys
 
 # Handle ModuleNotFoundError messages.
 try:
-    import pytube
+    from pytube import YouTube, Playlist
 except ModuleNotFoundError:
     print('Please install the required dependencies before running this program.')
     sys.exit(0)
 
+# If pyfiglet is not found, print out banner in regular text.
 try:
     import pyfiglet
     banner = pyfiglet.figlet_format('Youtube Video Downloader', font="slant")
     print(banner, end='\n\n')
 except ModuleNotFoundError:
     print('Youtube Video Downloader', end='\n\n')
+# -----------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------
+
+user_choice = option_menu()
+url = input('Enter your video url: ')
+
+
+def url_checker(url):
+    '''Make sure the entered url is valid. If it is return it.'''
+    try:
+        YouTube(url)
+    except:
+        print('\nInvalid url. Try again!')
+        sys.exit(0)
+    return url
+
+
+def download_video(url):
+    '''Download a single video and return its title.'''
+    video = YouTube(url)
+    video.streams.get_highest_resolution().download('Videos/')
+    return video.title
 
 
 def main():
-    pass
+    global url
+    global user_choice
+
+    match user_choice:
+        case 1:
+            url = url_checker(url) # checks url and returns it
+            title = download_video(url)
+            print(f"' {title} ' finished downloading.")
 
 
 if __name__ == '__main__':
--- menu.py
+++ menu.py
@@ -1,2 +1,16 @@
 def option_menu():
-    pass
+    '''A list of all the available options for the Youtube Video Downloader.'''
+    print('1. Download Youtube Video in highest resolution.')
+    print('2. Download Youtube Video Playlist.')
+    print()
+
+    # Make sure to handle any possible invalid user input.
+    while True:
+        try:
+            user_option = int(input('Please pick a number from the above menu: '))
+            if user_option < 1 or user_option > 5:
+                raise ValueError
+            break
+        except ValueError:
+            print('Please try entering a valid numeric value.\n')
+    return user_option
