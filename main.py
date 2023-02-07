f46f56d06c3328158ca4892eae930f068b1cd648
slasherjoe <slasher2033@protonmail.com>
2023-02-07 11:42:21 +0100

    a few minor additions

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ def main():
     '''Main body of the program.'''
     # Print out a title banner at the top of the program output.
     banner()
-    user_url = input('Enter your url: ')
-    path = input('Enter path to download your videos to: ')
+    user_url = input('Enter your url: ').strip()
+    path = input('Enter path to download your videos to: ').strip()
     url_type = 'video'
     if url_type == 'video':
         user_video = Video(user_url, path)
         user_video.download()
 
     if url_type == 'playlist':
-        pass
+        user_playlist = Video(user_url, path)
+        user_playlist.download_playlist()
 
 
 if __name__ == '__main__':
--- videos.py
+++ videos.py
@@ -9,23 +9,34 @@ class Video:
         '''Initialize url and path to the Video class.'''
         self.url = url
         self.path = path
-    
-    
+
+
     def get_url(self):
         '''Access and return your video url.'''
         return self.url
-    
-    
+
+
     def download(self):
-        '''Download Video into the given path.'''
+        '''Download Video into the provided path.'''
         try:
             YouTube(self.url)
         except VideoUnavailable:
             print('Video is not available.')
             sys.exit(0)
         except RegexMatchError:
+            print()
             print('The link does not seem to be quite right.')
             print('Correct the link and run the code again.')
             sys.exit(0)
         YouTube(self.url).streams.get_highest_resolution().download(self.path)
-        
+
+
+    def download_playlist(self):
+        '''Download Playlist into the provided path.'''
+        playlist = Playlist(self.url)
+        try:
+            for video in playlist.videos:
+                video.streams.get_highest_resolution().download(self.path)
+        except KeyError:
+            print()
+            print('Please check your link and try again.')
